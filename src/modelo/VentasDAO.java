4a24910c3aacdea23ba7519d40351e110a0429a6
Adrianabalceda <77200826+Adrianabalceda@users.noreply.github.com>
2023-06-01 16:45:34 -0500

    Refactorización VentasDAO.java
    Se agrega un manejo adecuado de las excepciones. Tipo de Refactorización: Extracción de Metodo 
    Mejora la legibilidad del código

--- src/modelo/VentasDAO.java
+++ src/modelo/VentasDAO.java
@@ -10,66 +10,100 @@ public class VentasDAO {
     Connection con;
     PreparedStatement ps;
     ResultSet rs;
-    int r=0;
-    
-    public String NroSerieVentas(){
-        String serie="";
-        String sql="select max(NumeroSerie) from ventas";
+    int r = 0;
+
+    public String NroSerieVentas() {
+        String serie = "";
+        String sql = "select max(NumeroSerie) from ventas";
         try {
-            con=cn.Conectar();
-            ps=con.prepareStatement(sql);
-            rs=ps.executeQuery();
+            con = cn.Conectar();
+            ps = con.prepareStatement(sql);
+            rs = ps.executeQuery();
             while (rs.next()) {
-                serie=rs.getString(1);
+                serie = rs.getString(1);
             }
         } catch (Exception e) {
+            // Manejo de la excepción
+            e.printStackTrace();
+        } finally {
+            closeResources();
         }
         return serie;
     }
-    
-    public String IdVentas(){
-        String idv="";
-        String sql="select max(IdVentas) from ventas";
+
+    public String IdVentas() {
+        String idv = "";
+        String sql = "select max(IdVentas) from ventas";
         try {
-            con=cn.Conectar();
-            ps=con.prepareStatement(sql);
-            rs=ps.executeQuery();
+            con = cn.Conectar();
+            ps = con.prepareStatement(sql);
+            rs = ps.executeQuery();
             while (rs.next()) {
-                idv=rs.getString(1);
+                idv = rs.getString(1);
             }
         } catch (Exception e) {
+            // Manejo de la excepción
+            e.printStackTrace();
+        } finally {
+            closeResources();
         }
         return idv;
     }
-    public int GuardarVentas(Ventas v){       
-        String sql="insert into Ventas(IdCliente, IdVendedor,NumeroSerie,FechaVentas,Monto,Estado)values(?,?,?,?,?,?)";
+
+    public int GuardarVentas(Ventas v) {
+        String sql = "insert into Ventas(IdCliente, IdVendedor,NumeroSerie,FechaVentas,Monto,Estado)values(?,?,?,?,?,?)";
         try {
-            con=cn.Conectar();
-            ps=con.prepareStatement(sql);
+            con = cn.Conectar();
+            ps = con.prepareStatement(sql);
             ps.setInt(1, v.getIdCliente());
             ps.setInt(2, v.getIdVendedor());
             ps.setString(3, v.getSerie());
             ps.setString(4, v.getFecha());
             ps.setDouble(5, v.getMonto());
             ps.setString(6, v.getEstado());
-            r=ps.executeUpdate();
+            r = ps.executeUpdate();
         } catch (Exception e) {
+            // Manejo de la excepción
+            e.printStackTrace();
+        } finally {
+            closeResources();
         }
-        
+
         return r;
     }
-    public int GuardarDetalleVentas(DetalleVentas dv){
-        String sql="insert into detalle_ventas(IdVentas,IdProducto,Cantidad,PrecioVenta)values(?,?,?,?)";
+
+    public int GuardarDetalleVentas(DetalleVentas dv) {
+        String sql = "insert into detalle_ventas(IdVentas,IdProducto,Cantidad,PrecioVenta)values(?,?,?,?)";
         try {
-            con=cn.Conectar();
-            ps=con.prepareStatement(sql);
+            con = cn.Conectar();
+            ps = con.prepareStatement(sql);
             ps.setInt(1, dv.getIdVentas());
             ps.setInt(2, dv.getIdProducto());
             ps.setInt(3, dv.getCantidad());
             ps.setDouble(4, dv.getPreVenta());
             ps.executeUpdate();
         } catch (Exception e) {
+            // Manejo de la excepción
+            e.printStackTrace();
+        } finally {
+            closeResources();
         }
         return r;
+    }
+
+    private void closeResources() {
+        try {
+            if (rs != null) {
+                rs.close();
+            }
+            if (ps != null) {
+                ps.close();
+            }
+            if (con != null) {
+                con.close();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
