578bd904bc3fb7d8a35b8f95bd4f4badaa564935
joseph-robles <75534573+joseph-robles@users.noreply.github.com>
2023-06-01 15:02:05 -0500

    Refactorizar método listarID
    Code Smell: Long Method; Técnica de Refactorización: Extract Method
    A "listarID" se le extrajo 1 método siendo este: "obtenerDatosCliente"

--- src/modelo/ClienteDAO.java
+++ src/modelo/ClienteDAO.java
@@ -14,26 +14,39 @@ public class ClienteDAO implements CRUD {
     ResultSet rs;
     Cliente co=new Cliente();
     
-    
-    public Cliente listarID(String dni){
-        Cliente c=new Cliente();
-       String sql="select * from cliente where Dni=?";
-        try {
-            con=cn.Conectar();
-            ps=con.prepareStatement(sql);
-            ps.setString(1, dni);
-            rs=ps.executeQuery();
-            while (rs.next()) {
-                c.setId(rs.getInt(1));
-                c.setDni(rs.getString(2));
-                c.setNom(rs.getString(3));
-                c.setDir(rs.getString(4));
-                c.setEstado(rs.getString(5));
-            }
-        } catch (Exception e) {
-        }
-        return c;
+    //Code Smell: Long Method; Técnica de Refactorización: Extract Method
+    //A "listarID" se le extrajo 1 método siendo este: "obtenerDatosCliente"
+    public Cliente listarID(String dni) {
+    Cliente c = new Cliente();
+    String sql = "select * from cliente where Dni=?";
+
+    try {
+        con = cn.Conectar();
+        ps = con.prepareStatement(sql);
+        ps.setString(1, dni);
+        rs = ps.executeQuery();
+        c = obtenerDatosCliente(rs);
+    } catch (Exception e) {
+        // Manejo de excepciones
     }
+
+    return c;
+}
+    //Método extraído desde "listarID"
+    private Cliente obtenerDatosCliente(ResultSet rs) throws SQLException {
+    Cliente c = new Cliente();
+
+    while (rs.next()) {
+        c.setId(rs.getInt(1));
+        c.setDni(rs.getString(2));
+        c.setNom(rs.getString(3));
+        c.setDir(rs.getString(4));
+        c.setEstado(rs.getString(5));
+    }
+
+    return c;
+}
+    
     //Metodos de Mantenimiento CRUD
     @Override
     public List listar() {
