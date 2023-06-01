c49937b009a9253c2014d3a191d76292bc7a78ff
Leonardo Dario Loaiza Sighuas <86206834+ldls1998@users.noreply.github.com>
2023-06-01 15:29:43 -0500

    Corrección de Long Method en clase ProductoDAO
    Se corrigió el code smell Long Method usando Exctract Method para evitar la redundancia y mejorar la legibilidad.

--- src/modelo/ProductoDAO.java
+++ src/modelo/ProductoDAO.java
@@ -1,52 +1,55 @@
-
 package modelo;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
 public class ProductoDAO {
+
     int r;
     PreparedStatement ps;
     ResultSet rs;
     Connection con;
     Conexion acceso = new Conexion();
-    Producto pro = new Producto();
-    
-   public int actualizarStock(int cant, int idp){
-       String sql="update producto set Stock=? where id=?";
-       try {
-           con=acceso.Conectar();
-           ps=con.prepareStatement(sql);
-           ps.setInt(1, cant);
-           ps.setInt(2, idp);
-           ps.executeUpdate();
-       } catch (Exception e) {
-       }
-       return r;
-   }
-    public Producto listarID(int id){
-        Producto p=new Producto();
-        String sql="select * from producto where id=?";
+
+    public int actualizarStock(int cant, int idp) throws SQLException {
+        String sql = "update producto set Stock=? where id=?";
         try {
-            con=acceso.Conectar();
-            ps=con.prepareStatement(sql);
+            con = acceso.Conectar();
+            ps = con.prepareStatement(sql);
+            ps.setInt(1, cant);
+            ps.setInt(2, idp);
+            ps.executeUpdate();
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            cerrarConexiones();
+        }
+        return r;
+    }
+
+    public Producto listarID(int id) {
+        Producto p = new Producto();
+        String sql = "select * from producto where id=?";
+        try {
+            con = acceso.Conectar();
+            ps = con.prepareStatement(sql);
             ps.setInt(1, id);
-            rs=ps.executeQuery();
+            rs = ps.executeQuery();
             while (rs.next()) {
-                p.setId(rs.getInt(1));
-                p.setNom(rs.getString(2));
-                p.setPre(rs.getDouble(3));
-                p.setStock(rs.getInt(4));
-                p.setEstado(rs.getString(5));
+                p = extraerProductoDeResultSet(rs);
             }
         } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            cerrarConexiones();
         }
         return p;
     }
-   
+
     //*************MANTENIMIENTO CRUD**************
     public List listarProducto() {
         String sql = "select * from producto";
@@ -56,15 +59,13 @@ public class ProductoDAO {
             ps = con.prepareStatement(sql);
             rs = ps.executeQuery();
             while (rs.next()) {
-                Producto p = new Producto();
-                p.setId(rs.getInt(1));
-                p.setNom(rs.getString(2));
-                p.setPre(rs.getDouble(3));
-                p.setStock(rs.getInt(4));
-                p.setEstado(rs.getString(5));
+                Producto p = extraerProductoDeResultSet(rs);
                 listaprod.add(p);
             }
         } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            cerrarConexiones();
         }
         return listaprod;
     }
@@ -80,6 +81,9 @@ public class ProductoDAO {
             ps.setString(4, p.getEstado());
             ps.executeUpdate();
         } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            cerrarConexiones();
         }
         return r;
     }
@@ -107,6 +111,9 @@ public class ProductoDAO {
             ps.setInt(5, p.getId());
             ps.executeUpdate();
         } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            cerrarConexiones();
         }
         return r;
     }
@@ -119,7 +126,35 @@ public class ProductoDAO {
             ps.setInt(1, id);
             ps.executeUpdate();
         } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            cerrarConexiones();
         }
         return r;
+    }
+
+    private Producto extraerProductoDeResultSet(ResultSet rs) throws SQLException {
+        int id = rs.getInt(1);
+        String nom = rs.getString(2);
+        double pre = rs.getDouble(3);
+        int stock = rs.getInt(4);
+        String estado = rs.getString(5);
+        return new Producto(id, nom, pre, stock, estado);
+    }
+
+    private void cerrarConexiones() {
+        try {
+            if (rs != null) {
+                rs.close();
+            }
+            if (ps != null) {
+                ps.close();
+            }
+            if (con != null) {
+                con.close();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
